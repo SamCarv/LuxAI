7fa9b1a82a0cb67dc4b568bd0716835ac3f0b06c
Fernando Cruz Cavina <fernando.cruz.cavina@gmail.com>
2026-05-27 17:03:40 -0300

    fix: duplicate AI section

--- frontend/src/components/user-settings-modal/sections/profile/index.tsx
+++ frontend/src/components/user-settings-modal/sections/profile/index.tsx
@@ -5,6 +5,7 @@ interface ProfileSectionProps {
     settings: UserUpdate
     setSettings: Dispatch<SetStateAction<UserUpdate>>
 }
+
 const ProfileSection = ({settings, setSettings}: ProfileSectionProps) => {
   return (
     <div className="space-y-4">
@@ -25,27 +26,8 @@ const ProfileSection = ({settings, setSettings}: ProfileSectionProps) => {
             <label className="text-sm font-medium text-slate-700 dark:text-white">New Password</label>
             <input type='password' className="border rounded-md p-2 w-full outline-candy-corn-400 dark:bg-zinc-800" placeholder="******" />
         </div>
-        <h3 className="text-lg font-semibold">Inteligência Artificial</h3>
-      
-      <div className="flex flex-col gap-2">
-        <label className="text-sm font-medium text-slate-700 dark:text-white">Provedor</label>
-        <select className="border rounded-md p-2 w-full outline-candy-corn-400 bg-white dark:bg-zinc-800" value={'google'} onChange={(ai_provider) => setSettings({...settings, ai_provider: ai_provider.target.value})}>
-          <option value="ollama">Ollama (Local)</option>
-          <option value="google">Google Gemini (Online)</option>
-        </select>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <label className="text-sm font-medium text-slate-700 dark:text-white">Chave API do provedor</label>
-        <input 
-          type="password" 
-          className="border rounded-md p-2 w-full outline-candy-corn-400 dark:bg-zinc-800" 
-          placeholder="sk-..."
-          onChange={(api_key) => setSettings({...settings, google_api_key: api_key.target.value})}
-        />
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileSection
+export default ProfileSection;
