4ba3378a39e7465c8fa6ac2e6db103df7e825354
Samuel Carvalho <samuelcarv@proton.me>
2026-05-26 22:44:45 -0300

    Fix user

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -1,14 +1,14 @@
-import type { UserCreate } from "../types/userDTO/userCreate"
-import { api } from "./api"
+import type { UserCreate } from "../types/userDTO/userCreate";
+import { api } from "./api";
 
-const ROUTE = "user"
+const ROUTE = "user";
 
 const get_user_by_id = async (id: string) => {
-    return api.get(`/${ROUTE}/${id}`)
-}
+  return api.get(`/${ROUTE}/${id}`);
+};
 
 const create_user = async (userCreate: UserCreate) => {
-    return api.post(`${ROUTE}`, userCreate)
-}
+  return api.post(`${ROUTE}/`, userCreate);
+};
 
-export { get_user_by_id, create_user }
+export { get_user_by_id, create_user };
